d624e2d2fcc1f2fbd7e0cf08737665c70f657512
Kratosff <135919071+Kratosff@users.noreply.github.com>
2024-10-20 16:59:46 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=W7AXFLiL#BRHaxhyolI5cBNnfpeQm_8IHdDR46iMHdbx8ws8mMAk",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
